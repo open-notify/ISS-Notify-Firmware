72e8f66bd214b46f8d9045bf671465cbbf2cb1ff
Nathan Bergey <nathan.bergey@gmail.com>
2012-07-23 00:38:38 -0700

    experimenting with simple serial comms

--- ISS-Notify.c
+++ ISS-Notify.c
@@ -31,14 +31,10 @@
 #include <util/delay.h>
 #include <avr/pgmspace.h>
 #include <stdio.h>
-//#include "library/usb_serial.h"
 #include "led.h"
 #include "analog.h"
-//#include "usb.h"
 #include "rtc.h"
-
 #include "Descriptors.h"
-
 #include "library/LUFA/Version.h"
 #include "library/LUFA/Drivers/USB/USB.h"
 
@@ -66,7 +62,6 @@ static FILE USBSerialStream;
  *  passed to all CDC Class driver functions, so that multiple instances of the same class
  *  within a device can be differentiated from one another.
  */
-
 USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface =
 	{
 		.Config =
@@ -94,24 +89,28 @@ void EVENT_USB_Device_ControlRequest(void);
 
 		
 // Measure battery voltage
-int get_battery_voltage(void);
+uint8_t get_battery_voltage(void);
 
 int main(void)
 {
-  int i,j,k;
+  uint8_t batv;
+  int j,k;
   
+  uint32_t timelapse;
+
 	Setup_Hardware();
 	
 	unsigned int battery_wheel[12] = {8,  8,  24, 40, 39, 55, 70, 99, 131, 130, 129, 128};
 
   /* Create a regular character stream for the interface so that it can be used with the stdio.h functions */
 	CDC_Device_CreateStream(&VirtualSerial_CDC_Interface, &USBSerialStream);
+	
   // Enable interupts
   sei();
 
-  i = get_battery_voltage();
+  batv = get_battery_voltage();
   unsigned int show[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
-  for (j=0;j<=i;j++)
+  for (j=0;j<=batv;j++)
   {
      for (k=0;k<=j;k++)
        show[k] = battery_wheel[k];
@@ -129,114 +128,93 @@ int main(void)
   
   while(1) {
   
-    CDC_Device_SendString(&VirtualSerial_CDC_Interface, "messages\n");
-
+    /*
+    if (timelapse > 100000)
+    {
+      timelapse = 0;
+      
+      uint32_t time = get_time();
+      char printbuf[32];
+		  int len = sprintf(printbuf, "s: %lu \n", time);
+      fputs(printbuf, &USBSerialStream);
+    }
+    //fputs("message\n", &USBSerialStream);
+    //CDC_Device_SendString(&VirtualSerial_CDC_Interface, "message\n");
+    */
+    
 		/* Must throw away unused bytes from the host, or it will lock up while waiting for the device */
-		CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
-
+		//CDC_Device_ReceiveByte(&VirtualSerial_CDC_Interface);
+		/*
+    uint8_t ColourUpdate = fgetc(&USBSerialStream);
+    
+    if (ColourUpdate == 97) {
+      unsigned int col = fgetc(&USBSerialStream);
+      unsigned int show[12] = {col,col,col,col,col,col,col,col,col,col,col,col};
+      set_data(show);
+    }
+    */
+    int n;
+    int read = fscanf (&USBSerialStream,"c%d",&n);
+    if (read > 0) {
+      unsigned int show[12] = {n,n,n,n,n,n,n,n,n,n,n,n};
+      set_data(show);
+    }
+    
 		CDC_Device_USBTask(&VirtualSerial_CDC_Interface);
 		USB_USBTask();
 		
-		//_delay_ms(1000);
+		timelapse++;
 		;
   }
-  /*
-	while (1) {
-		// wait for the user to run their terminal emulator program
-		// which sets DTR to indicate it is ready to receive.
-		while (!(usb_serial_get_control() & USB_SERIAL_DTR)) ; //wait
-
-		// discard anything that was received prior.  Sometimes the
-		// operating system or other software will send a modem
-		// "AT command", which can still be buffered.
-		usb_serial_flush_input();
-
-		// print a nice welcome message
-		send_str(PSTR("Begin\n"));
-		
-		/*
-    time testt;
-    
-    testt.year   = 12;
-    testt.month  = 07;
-    testt.day    = 21;
-    testt.hour   = 18;
-    testt.minute = 30;
-    testt.second = 55;
-    
-    //reset_rtc();
-    //set_time(testt);
-    
-    
-		while (1) {
-		  
-		  send_str(PSTR("clock talk\n"));
-		  
-		  uint32_t time = get_time();
-		  
-		  //uint8_t setup = read_address(0x03);
-		  
-		  char printbuf[32];
-		  int len = sprintf(printbuf, "s: %lu", time);
-		  int l;
-		  for (l=0;l<len;l++){
-		    usb_serial_putchar(printbuf[l]);
-		  }
-		  send_str(PSTR("\n"));
-		  
-		  _delay_ms(10000);
-		  
-    }
-	}
-	*/
 }
 
 /** Event handler for the library USB Connection event. */
-
 void EVENT_USB_Device_Connect(void)
 {
   //LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
+  //unsigned int show[12] = {0,0,0,0,0,8,0,0,0,0,0,0};
+  //set_data(show);
 	return;
 }
 
-
 /** Event handler for the library USB Disconnection event. */
-
 void EVENT_USB_Device_Disconnect(void)
 {
   //LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+  //unsigned int show[12] = {0,0,0,0,0,8,0,0,0,0,0,0};
+  //set_data(show);
 	return;
 }
 
-
 /** Event handler for the library USB Configuration Changed event. */
-
 void EVENT_USB_Device_ConfigurationChanged(void)
 {
 	bool ConfigSuccess = true;
 
 	ConfigSuccess &= CDC_Device_ConfigureEndpoints(&VirtualSerial_CDC_Interface);
+	
+	unsigned int show[12] = {0,0,0,0,0,0,0,0,0,2048,0,0};
+  set_data(show);
 
 	//LEDs_SetAllLEDs(ConfigSuccess ? LEDMASK_USB_READY : LEDMASK_USB_ERROR);
 }
 
-
 /** Event handler for the library USB Control Request reception event. */
-
 void EVENT_USB_Device_ControlRequest(void)
 {
 	CDC_Device_ProcessControlRequest(&VirtualSerial_CDC_Interface);
+	
+	//unsigned int show[12] = {0,0,0,0,0,0,0,0,0,2048,0,0};
+  //set_data(show);
 }
 
-
-
-int get_battery_voltage(void)
+uint8_t get_battery_voltage(void)
 {
     // Enable battery measurment
     PORTB |=  1;
     
     // Read voltage on battery sense pin
-	  int val = adc_read(BAT_SENSE);
+	  uint16_t val = adc_read(BAT_SENSE);
 	  
 	  // 0 isn't 0 V, it's the miniumum battery volage through voltage divider
 	  if (val >= MIN_BAT_ADC)
@@ -282,27 +260,8 @@ void Setup_Hardware(void)
 	MCUSR &= ~(1 << WDRF);
 	wdt_disable();
 
-	/* Disable clock division */
-	//clock_prescale_set(clock_div_1);
-
 	/* Hardware Initialization */
 	USB_Init();
-
-	/* Timer Initialization */
-//	OCR0A  = 100;
-//	TCCR0A = (1 << WGM01);
-//	TCCR0B = (1 << CS00);
-//	TIMSK0 = (1 << OCIE0A);
-	
-	// END LUFA USB
-	
-	
-	/*
-	// USB
-	usb_init();
-	while (!usb_configured()) ; // wait
-	//_delay_ms(500);
-	*/
 	
 	// ready clock
 	rtc_init();
@@ -312,7 +271,8 @@ void Setup_Hardware(void)
   
   cli();
   
-	TIMSK1 = _BV(OCIE1A);
+  /* Timer Initialization */
+  TIMSK1 = _BV(OCIE1A);
 	TCCR1B |=
     // Prescaler divides the clock rate by 256.
     (_BV(CS12) )  |
@@ -322,6 +282,19 @@ void Setup_Hardware(void)
     
   // 8 MHz clock. 1024 prescaler. Counting to 7812 is one second.
   OCR1A = 1;
+   
+  /*
+	TIMSK3 = _BV(OCIE3A);
+	TCCR3B |=
+    // Prescaler divides the clock rate by 256.
+    (_BV(CS32) )  |
+    // Set WGM12 bit to clear timer on compare with the OCR1A
+    // register.
+    _BV(WGM32);
+    
+  // 8 MHz clock. 1024 prescaler. Counting to 7812 is one second.
+  OCR3A = 100;
+  */
   
   BLANK_LOW;
 }
