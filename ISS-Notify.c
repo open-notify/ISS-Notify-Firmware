91da2c497af46f93f1736cc1ca7289450b5b8fc7
Nathan Bergey <nathan.bergey@gmail.com>
2013-01-18 18:17:51 -0800

    Refactoring commands

--- ISS-Notify.c
+++ ISS-Notify.c
@@ -49,6 +49,11 @@
 void Setup_Hardware(void);
 uint32_t millis(void);
 
+void ack(void);
+void say_ms(void);
+void set_color(void);
+void say_time(void);
+
 /** Standard file stream for the CDC interface when set up, so that the virtual CDC COM port can be
  *  used like any regular character stream in the C APIs
  */
@@ -90,142 +95,154 @@ volatile uint32_t ms         = 0;
 
 // EEPROM Storage
 uint16_t EEMEM NonVolatileColor;
-ipass EEMEM ISS_Passes[10];
+//uint8_t EEMEM NonVolatileOptions;
+uint8_t EEMEM NumOfStoredISSPasses;
+ipass EEMEM StoredISSPasses[50];
 
+// Global variable that always stores current color
 unsigned int show[12] = {0,0,0,0,0,0,0,0,0,0,0,0};
 
+
+// Commands:
+static const cmd COMMANDS[4] = {  {c: 'a', resp: ack},
+								  {c: 'm', resp: say_ms},
+								  {c: 'c', resp: set_color},
+								  {c: 't', resp: say_time},
+							   };
+
+
+/*******************************************************************************
+* INTERRUPTS
+*******************************************************************************/
 
 /**
  * Wake up on alarm
  */
 ISR(INT3_vect) 
 {
-  // Disable interupt
-  EIMSK &= ~(1<<INT3);
-  
-  //unsigned int show[12] = {0,8,0,8,0,128,0,8,0,8,0,8};
-  //set_data(show);
-  //reset_alarm = 1;
-  alarm = 1;
+	// Disable interupt
+	EIMSK &= ~(1<<INT3);
+
+	//unsigned int show[12] = {0,8,0,8,0,128,0,8,0,8,0,8};
+	//set_data(show);
+	//reset_alarm = 1;
+	alarm = 1;
 }
 
-// milli timer
+/**
+ * Counting milliseconds
+ */
 ISR(TIMER3_COMPA_vect)
 {
-  ms++;
-} 
-
-// milli get
-uint32_t millis(void) 
-{ 
-  return ms; 
+	ms++;
 }
 
+
+/*******************************************************************************
+* MAIN
+*******************************************************************************/
 int main(void)
 {
-  uint8_t batv;
-  int i,j,k,a;
-  uint32_t premills = 0;
+	int i;
 
+	// Setup hardware
 	Setup_Hardware();
-   
-	unsigned int battery_wheel[12] = {8,  8,  24, 40, 39, 55, 70, 99, 131, 130, 129, 128};
 
-  /* Create a regular character stream for the interface so that it can be used with the stdio.h functions */
+	//unsigned int battery_wheel[12] = {8,  8,  24, 40, 39, 55, 70, 99, 131, 130, 129, 128};
+
+	/**
+	 * Setting up USB communication
+	 * Create a regular character stream for the interface so that 
+	 * it can be used with the stdio.h functions
+	 */
 	CDC_Device_CreateStream(&VirtualSerial_CDC_Interface, &USBSerialStream);
-	
-  // Enable interupts
-  sei();
 
-  //batv = get_battery_voltage();
-    //for (j=0;j<=batv;j++)
-  //{
-  //   for (k=0;k<=j;k++)
-  //     show[k] = battery_wheel[k];
-  //   set_data(show);
-  //   _delay_ms(8);
-  //}
-  //_delay_ms(400);
-  
-  //for (j=11;j>=0;j--)
-  //{
-  //   show[j] = 0;
-  //   set_data(show);
-  //   _delay_ms(8);
-  //}
-  
-  //for (i=0;i<12;i++) {
-  // show[i] = 0;
-  //}
 
-    uint16_t stored_color;
+	/**
+	 * TODO: Sanity Check
+	 */
+	// Reset alarms
+	// What time is it
+	// Are we inside a pass?
+	//     true: show pass
+	//     false: set alarm for next pass
 
-    stored_color = eeprom_read_word(&NonVolatileColor);
-    for (i=0;i<12;i++) {
-        show[i] = (int) stored_color;
-        set_data(show);
-        _delay_ms(20);
-    }
-    _delay_ms(100);
-    for (i=11;i>=0;i--) {
-        show[i] = 0;
-        set_data(show);
-        _delay_ms(20);
-    }
 
-  a = 0;
-  while(1) {
- 
-    // Color update
-    int n;
-    int read = fscanf(&USBSerialStream,"c%d",&n);
-    if (read > 0) {
-      for (i=0;i<12;i++) {
-        show[i] = n;
-      }
-      set_data(show);
-    }
-    
-    // knock knock
-    char hello;
-    if (fscanf(&USBSerialStream,"hello%c", &hello) == 1) {
-      // whos there?
-      fputs("hi", &USBSerialStream);
-    }
-    
-    if (fscanf(&USBSerialStream,"ms%c", &hello) == 1) {
-      // whos there?
-      fprintf(&USBSerialStream, "%ul", millis());
-    }
+	// Enable interupts
+	sei();
 
-    // Set color
-    int color;
-    if (fscanf(&USBSerialStream,"setc%d", &color) == 1) {
-         eeprom_update_word((uint16_t*) &NonVolatileColor, (uint16_t)color);  
-    }
+
+	// Wake up animation:
+	uint16_t stored_color;
+	stored_color = eeprom_read_word(&NonVolatileColor);
+	for (i=0;i<12;i++) {
+		show[i] = (int) stored_color;
+		set_data(show);
+		_delay_ms(20);
+	}
+	_delay_ms(100);
+	for (i=11;i>=0;i--) {
+		show[i] = 0;
+		set_data(show);
+		_delay_ms(20);
+	}
+
+
+	/***************************************************************************
+	* THE MAIN LOOP
+	***************************************************************************/
+	while(1) {
+
+		// Listen for commands over USB serial
+		char c;
+		if (fscanf(&USBSerialStream,"ISS%c\n", &c) == 1) {
+			for (i=0;i<4;i++) {
+				cmd command = COMMANDS[i];
+				if (command.c == c) {
+					command.resp();
+					break;
+				}
+			}
+		}
+
+		/*
+		// Color update
+		int n;
+		int read = fscanf(&USBSerialStream,"c%d",&n);
+		if (read > 0) {
+			for (i=0;i<12;i++) {
+				show[i] = n;
+			}
+		set_data(show);
+		}
 
     // Set block
-    int tvalue;
+    int npasses;
     ipass block[10];
-    if (fscanf(&USBSerialStream, "z%d", &tvalue) == 1) {
-        block[0].time = 1234567;
-        block[0].duration = 500;
-        eeprom_update_block((const void * ) &block, (void*) &ISS_Passes, sizeof(ipass));        
+    if (fscanf(&USBSerialStream, "z%d\n", &npasses) == 1) {
+        
+        for (i=0;i<npasses;i++) {
+            uint32_t t;
+            uint16_t d;
+            ipass p;
+            
+            fscanf(&USBSerialStream, "%lu,%u,", &t, &d);
+            p.time = t;
+            p.duration = d;
+            block[i] = p;
+        }
+
+        eeprom_update_block((const void * ) &block, (void*) &ISS_Passes, sizeof(ipass)*npasses);        
     }
 
     // Get block
     if (fscanf(&USBSerialStream,"getvalue%c", &hello) == 1) {
-        eeprom_read_block((void*) &block, (const void *) &ISS_Passes, sizeof(ipass));
-        fprintf(&USBSerialStream, "%lu - %u", block[0].time, block[0].duration);
+        eeprom_read_block((void*) &block, (const void *) &ISS_Passes, sizeof(ipass)*10);
+        for (i=0;i<10;i++) {
+            fprintf(&USBSerialStream, "%lu - %u\n", block[i].time, block[i].duration);
+        }
     }
 
-    // get time
-    char readtime;
-    if (fscanf(&USBSerialStream,"t%c", &readtime) == 1) {
-      time now = get_time();
-      fprintf(&USBSerialStream, "Y%02dM%02dD%02dH%02dM%02dS%02d", now.year, now.month, now.day, now.hour, now.minute, now.second);
-    }
-    
     // set time
     int year, month, day, hour, min, sec;
     if (fscanf(&USBSerialStream,"TY%dM%dD%dH%dM%dS%d", &year, &month, &day, &hour, &min, &sec) == 6) {
@@ -291,6 +308,7 @@ int main(void)
         premills = curmills;
       }
     }
+	*/
     
     // When an alarm is triggered by the RTC:
     if (alarm > 0)
@@ -320,29 +338,67 @@ int main(void)
   }
 }
 
+/**
+ * Commands:
+ */
+void ack(void)
+{
+	fputs("ack", &USBSerialStream);
+}
+
+void say_ms(void)
+{
+	fprintf(&USBSerialStream, "%lu", millis());
+}
+
+void set_color(void)
+{
+	int color;
+	if (fscanf(&USBSerialStream,"%d", &color) == 1) {
+		eeprom_update_word((uint16_t*) &NonVolatileColor, (uint16_t)color);
+		fputs("set", &USBSerialStream);
+	}
+	else
+		fputs("err", &USBSerialStream);
+}
+
+void say_time(void)
+{
+	time now = get_time();
+	fprintf(&USBSerialStream, "Y%02dM%02dD%02dH%02dM%02dS%02d", now.year, now.month, now.day, now.hour, now.minute, now.second);
+}
+
+/**
+ * Helper funtion to get current number of milliseconds since boot
+ * ms is volitile and updated in a timer
+ */
+uint32_t millis(void) { return ms; }
+
+
 /** Event handler for the library USB Connection event. */
 void EVENT_USB_Device_Connect(void)
 {
-  //LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
-  //unsigned int show[12] = {0,0,0,0,0,8,0,0,0,0,0,0};
-  //set_data(show);
+	//LEDs_SetAllLEDs(LEDMASK_USB_ENUMERATING);
+	//unsigned int show[12] = {0,0,0,0,0,8,0,0,0,0,0,0};
+	//set_data(show);
 	return;
 }
+
 
 /** Event handler for the library USB Disconnection event. */
 void EVENT_USB_Device_Disconnect(void)
 {
-  //LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
-  //unsigned int show[12] = {0,0,0,0,0,8,0,0,0,0,0,0};
-  //set_data(show);
+	//LEDs_SetAllLEDs(LEDMASK_USB_NOTREADY);
+	//unsigned int show[12] = {0,0,0,0,0,8,0,0,0,0,0,0};
+	//set_data(show);
 	return;
 }
+
 
 /** Event handler for the library USB Configuration Changed event. */
 void EVENT_USB_Device_ConfigurationChanged(void)
 {
 	bool ConfigSuccess = true;
-
 	ConfigSuccess &= CDC_Device_ConfigureEndpoints(&VirtualSerial_CDC_Interface);
 	
     //unsigned int show[12] = {0,0,0,0,0,0,0,0,0,2048,0,0};
@@ -355,37 +411,43 @@ void EVENT_USB_Device_ConfigurationChanged(void)
 void EVENT_USB_Device_ControlRequest(void)
 {
 	CDC_Device_ProcessControlRequest(&VirtualSerial_CDC_Interface);
-	
 	//unsigned int show[12] = {0,0,0,0,0,0,0,0,0,2048,0,0};
-  //set_data(show);
+  	//set_data(show);
 }
 
-
+/**
+ * Hardware set up. Most things are done in library code.
+ */
 void Setup_Hardware(void)
 {
-  // set for 8 MHz clock (see Makefile)
+	// set for 8 MHz clock (see Makefile)
 	CPU_PRESCALE(0);
-	
-  // JTAG Disable
-  MCUCR = (1 << JTD); 
+
+	// JTAG Disable
 	MCUCR = (1 << JTD); 
-	
-	/* Disable watchdog if enabled by bootloader/fuses */
+	MCUCR = (1 << JTD); 
+
+	// Disable watchdog if enabled by bootloader/fuses
 	MCUSR &= ~(1 << WDRF);
 	wdt_disable();
-	
-	cli();
-	
-  LED_Init();
 
+	// Disable Interupts
+	cli();
+
+	// Initilise pins and interupts for LED driver
+	LED_Init();
+
+	// Initilise USB
 	USB_Init();
 
+	// Initilise pins and interupts for talking to RTC
 	RTC_Init();
-	
+
+	// Ininilise pins for sensing battary volatge and charge state
 	charge_Init();
 
-  // milli timer
-  OCR3AL = 8;
-  TCCR3B = (1<<WGM32)|(5<<CS30); //ctc, div1024
-  TIMSK3 = (1<<OCIE3A); //compa irq
+	// set up millisecond timer
+	OCR3AL = 8;
+	TCCR3B = (1<<WGM32)|(5<<CS30); //ctc, div1024
+	TIMSK3 = (1<<OCIE3A); //compa irq
 }
--- structs.h
+++ structs.h
@@ -2,6 +2,16 @@
  * Defines on ISS overhead pass
  */
 typedef struct {
-    uint32_t time;      // Time that the ISS rises [unix time]
-    uint16_t duration;  // Set time - rise time [seconds]
+	uint32_t time;      // Time that the ISS rises [unix time]
+	uint16_t duration;  // Set time - rise time [seconds]
 } ipass;
+
+
+/**
+ * Commands that ISS Notify understands
+ * contains a test character and a function to call if matched
+ */
+typedef struct {
+	char c;             // Test chararacter
+	void (*resp)(void); // Response function
+} cmd;
